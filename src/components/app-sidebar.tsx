e1c90795f8b37160d086872809a80f2fd424e4ca
dldri <diovian.liu@gmail.com>
2025-05-23 22:22:48 +0800

    laid out sidebar menu structure

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -3,18 +3,55 @@ import {
     SidebarContent,
     SidebarFooter,
     SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
     SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
 } from "@/components/ui/sidebar"
+import { LayoutDashboard } from "lucide-react"
+
+const data = {
+    navMain: [
+        {
+            title: "Refurbishment Services",
+            url: "#",
+            items: [
+                {
+                    title: "Dashboard",
+                    url: "#",
+                    icon: LayoutDashboard,
+                },
+            ],
+        },
+    ],
+}
 
 export function AppSidebar() {
     return (
-        <Sidebar>
+        <Sidebar /*{...props}*/>
             <SidebarHeader className="inline-flex">
                 PROK | Data Hive
             </SidebarHeader>
             <SidebarContent>
-                <SidebarGroup />
-                <SidebarGroup />
+                {}
+                {data.navMain.map((item) => (
+                    < SidebarGroup key={item.title} >
+                        <SidebarGroupLabel>{item.title}</SidebarGroupLabel>
+                        <SidebarGroupContent>
+                            <SidebarMenu>
+                                {item.items.map((item) => (
+                                    <SidebarMenuItem key={item.title}>
+                                        <SidebarMenuButton asChild >
+                                            <a href={item.url}>{item.title}</a>
+                                        </SidebarMenuButton>
+                                    </SidebarMenuItem>
+                                ))}
+                            </SidebarMenu>
+                        </SidebarGroupContent>
+                    </SidebarGroup>
+                ))}
             </SidebarContent>
             <SidebarFooter />
         </Sidebar>
