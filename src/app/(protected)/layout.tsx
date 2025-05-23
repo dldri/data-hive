d65ea7da785b32a8e162d6220dddbae9cbee8ac5
dldri <diovian.liu@gmail.com>
2025-05-23 20:43:01 +0800

    added sidebar header

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,8 +1,14 @@
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
+
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
     return (
         <div>
-            <h1>Hello World</h1>
-            {children}
+            <SidebarProvider>
+                <AppSidebar />
+                <SidebarTrigger />
+                {children}
+            </SidebarProvider>
         </div>
     )
 }
---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,22 @@
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarGroup,
+    SidebarHeader,
+} from "@/components/ui/sidebar"
+
+export function AppSidebar() {
+    return (
+        <Sidebar>
+            <SidebarHeader className="inline-flex">
+                PROK | Data Hive
+            </SidebarHeader>
+            <SidebarContent>
+                <SidebarGroup />
+                <SidebarGroup />
+            </SidebarContent>
+            <SidebarFooter />
+        </Sidebar>
+    )
+}
